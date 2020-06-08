3631696be3dd27dfca907541dc5efb826180dc53
Lennert Vandecasteele <lennert.vandecasteele@outlook.com>
2020-06-08 22:35:08 +0200

    Make simple api/ route

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+require("dotenv").config();
+const express = require("express");
+const server = express();
+const routes = require("./routes");
+
+server.use("/api", routes);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`Server is up and listening on port ${port}`));
