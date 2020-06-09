c24d381cab58ffbca8dae3eed6978197ca34941c
Lennert Vandecasteele <lennert.vandecasteele@outlook.com>
2020-06-09 02:09:29 +0200

    Move everything to root of project

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
-const routes = require("./app/routes");
+const server = express();
+const routes = require("./routes");
 
-app.use("/api", routes);
+server.use("/api", routes);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server is up and listening on port ${port}`));
+server.listen(port, () => console.log(`Server is up and listening on port ${port}`));
