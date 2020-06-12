dfedfe7f0f5b5fd25227a33f4213074f4e5a4948
Lennert Vandecasteele <lennert.vandecasteele@outlook.com>
2020-06-12 20:42:30 +0200

    Refactor module.exports

--- controllers/webhooks.js
+++ controllers/webhooks.js
@@ -1,31 +1,35 @@
 const Webhook = require("../models").Webhook;
 
-module.exports = {
-    getAll: async (req, res) => {
-        try {
-            const webhooks = await Webhook.findAll();
-            res.status(200).send(webhooks);
-        } catch (error) {
-            if(process.env.NODE_ENV === "production") {
-                res.status(400).json({error: "Could not get all posts."})
-            } else {
-                res.status(400).send(error);
-            }
-        }
-    },
-    add: async (req, res) => {
-        try {
-            const webhook = await Webhook.create({
-                url: req.body.url
-            });
-
-            res.status(201).send(webhook);
-        } catch (error) {
-            if (process.env.NODE_ENV === "production") {
-                res.status(400).json({ error: "Could not create post." })
-            } else {
-                res.status(400).send(error);
-            }
+async function getAll(req, res) {
+    try {
+        const webhooks = await Webhook.findAll();
+        res.status(200).send(webhooks);
+    } catch (error) {
+        if(process.env.NODE_ENV === "production") {
+            res.status(400).json({error: "Could not get all posts."})
+        } else {
+            res.status(400).send(error);
         }
     }
+}
+
+async function add(req, res) {
+    try {
+        const webhook = await Webhook.create({
+            url: req.body.url
+        });
+
+        res.status(201).send(webhook);
+    } catch (error) {
+        if (process.env.NODE_ENV === "production") {
+            res.status(400).json({ error: "Could not create post." })
+        } else {
+            res.status(400).send(error);
+        }
+    }
+}
+
+module.exports = {
+    getAll,
+    add
 };
