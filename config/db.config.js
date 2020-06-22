dc6537224c9250e2bd14d16df3f166af2159666b
Lennert Vandecasteele <lennert.vandecasteele@outlook.com>
2020-06-22 18:53:26 +0200

    Fix sequelize error production

--- config/db.config.js
+++ config/db.config.js
@@ -24,6 +24,9 @@ module.exports = {
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOSTNAME,
     "dialect": "mysql",
+    "define": {
+      "timestamps": false
+    },
     "logging": false,
     "pool": {
       "max": 5,
