ac986f1dc07abe6b5f0f7c3f4ddde4489b90b69b
Lennert Vandecasteele <lennert.vandecasteele@outlook.com>
2021-01-18 02:32:12 +0100

    Deleted old routes

--- routes/posts.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const postsController = require("../controllers").posts;
-
-router.get("/", postsController.getAll);
-router.get("/latest", postsController.getLatest);
-router.post("/", postsController.add);
-
-module.exports = router;
--- routes/webhooks.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-const webhooksController = require("../controllers").webhooks;
-
-router.get("/", webhooksController.getAll);
-router.post("/", webhooksController.add);
-
-module.exports = router;
